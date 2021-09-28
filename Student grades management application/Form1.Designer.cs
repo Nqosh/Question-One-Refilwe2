a54a0b1a67890eee5d6d3518dd385b840f3ace26
Nqobile <nqobilemoyo@capitecbank.co.za>
2021-09-28 11:58:25 +0200

    First Commit

---
+++ Student grades management application/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace Student_grades_management_application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.txtfileName = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtStudentNo = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtIndividualAssignment = new System.Windows.Forms.TextBox();
+            this.txtGrpAssignment = new System.Windows.Forms.TextBox();
+            this.txtTest = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnAddToFile = new System.Windows.Forms.Button();
+            this.dgvStudents = new System.Windows.Forms.DataGridView();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(12, 19);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(71, 17);
+            this.lblFileName.TabIndex = 0;
+            this.lblFileName.Text = "File Name";
+            // 
+            // txtfileName
+            // 
+            this.txtfileName.Location = new System.Drawing.Point(94, 19);
+            this.txtfileName.Name = "txtfileName";
+            this.txtfileName.Size = new System.Drawing.Size(100, 22);
+            this.txtfileName.TabIndex = 1;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(241, 14);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(178, 32);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Student No";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Surname";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 279);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Indv Assignment";
+            // 
+            // txtStudentNo
+            // 
+            this.txtStudentNo.Location = new System.Drawing.Point(143, 75);
+            this.txtStudentNo.Multiline = true;
+            this.txtStudentNo.Name = "txtStudentNo";
+            this.txtStudentNo.Size = new System.Drawing.Size(325, 34);
+            this.txtStudentNo.TabIndex = 7;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(143, 136);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(325, 34);
+            this.txtName.TabIndex = 8;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(143, 195);
+            this.txtSurname.Multiline = true;
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(325, 34);
+            this.txtSurname.TabIndex = 9;
+            // 
+            // txtIndividualAssignment
+            // 
+            this.txtIndividualAssignment.Location = new System.Drawing.Point(143, 262);
+            this.txtIndividualAssignment.Multiline = true;
+            this.txtIndividualAssignment.Name = "txtIndividualAssignment";
+            this.txtIndividualAssignment.Size = new System.Drawing.Size(325, 34);
+            this.txtIndividualAssignment.TabIndex = 10;
+            // 
+            // txtGrpAssignment
+            // 
+            this.txtGrpAssignment.Location = new System.Drawing.Point(143, 332);
+            this.txtGrpAssignment.Multiline = true;
+            this.txtGrpAssignment.Name = "txtGrpAssignment";
+            this.txtGrpAssignment.Size = new System.Drawing.Size(325, 34);
+            this.txtGrpAssignment.TabIndex = 11;
+            // 
+            // txtTest
+            // 
+            this.txtTest.Location = new System.Drawing.Point(143, 387);
+            this.txtTest.Multiline = true;
+            this.txtTest.Name = "txtTest";
+            this.txtTest.Size = new System.Drawing.Size(325, 34);
+            this.txtTest.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 335);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Grp Assignment";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(31, 404);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Test";
+            // 
+            // btnAddToFile
+            // 
+            this.btnAddToFile.Location = new System.Drawing.Point(143, 451);
+            this.btnAddToFile.Name = "btnAddToFile";
+            this.btnAddToFile.Size = new System.Drawing.Size(204, 42);
+            this.btnAddToFile.TabIndex = 16;
+            this.btnAddToFile.Text = " Add To File";
+            this.btnAddToFile.UseVisualStyleBackColor = true;
+            // 
+            // dgvStudents
+            // 
+            this.dgvStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudents.Location = new System.Drawing.Point(527, 75);
+            this.dgvStudents.Name = "dgvStudents";
+            this.dgvStudents.RowHeadersWidth = 51;
+            this.dgvStudents.RowTemplate.Height = 24;
+            this.dgvStudents.Size = new System.Drawing.Size(568, 418);
+            this.dgvStudents.TabIndex = 17;
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(678, 14);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(229, 38);
+            this.btnDisplay.TabIndex = 18;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 573);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.dgvStudents);
+            this.Controls.Add(this.btnAddToFile);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtTest);
+            this.Controls.Add(this.txtGrpAssignment);
+            this.Controls.Add(this.txtIndividualAssignment);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtStudentNo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtfileName);
+            this.Controls.Add(this.lblFileName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.TextBox txtfileName;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtStudentNo;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtIndividualAssignment;
+        private System.Windows.Forms.TextBox txtGrpAssignment;
+        private System.Windows.Forms.TextBox txtTest;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnAddToFile;
+        private System.Windows.Forms.DataGridView dgvStudents;
+        private System.Windows.Forms.Button btnDisplay;
+    }
+}
+
---
+++ Student grades management application/Form1.cs
@@ -0,0 +1,64 @@
+﻿using DocumentFormat.OpenXml.ExtendedProperties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student_grades_management_application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnCreate_Click(object sender, EventArgs e)
+        {   
+                string fileName = txtfileName.Text;
+                string studeNo = txtStudentNo.Text;
+                string Name = txtName.Text;
+                string surName = txtSurname.Text;
+                string indvAssigment = txtIndividualAssignment.Text;
+                string grpAssignment = txtGrpAssignment.Text;
+                string test = txtTest.Text;  
+
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                              fileName + " " + ".txt");
+
+            List<string> ls = new List<string>();
+            ls.Add(fileName);
+            ls.Add(studeNo);
+            ls.Add(Name);
+            ls.Add(surName);
+            ls.Add(indvAssigment);
+            ls.Add(grpAssignment);
+            ls.Add(test);
+
+            StringBuilder words = new StringBuilder();
+            foreach(var item in ls)
+            {
+                words.Append(item);
+            }
+            var test2 = words.ToString();
+
+
+            using (StreamWriter outputFile = new StreamWriter(filePath, true)) //// true to append data to the file
+            {
+                await outputFile.WriteAsync(test2);
+            }
+
+
+            //txtfileName.Text = "";
+            //txtStudentNo.Text = "";
+            //txtName.Text = "";
+            //txtSurname = "";
+        }
+    }
+}
